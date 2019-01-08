d0f61a67ab60fae0e661d107aaf3ee8080afaeb1
Jordan Heffernan <jheff2@gmail.com>
2019-01-08 14:28:19 -0800

    updated find to search both title and content and changed schema to remove unecesseary info from response

--- models/note.js
+++ models/note.js
@@ -9,4 +9,12 @@ const noteSchema = new mongoose.Schema({
 
 noteSchema.set('timestamps', true);
 
+noteSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
 module.exports = mongoose.model('Note', noteSchema);
